ec41d67c5d72f6bee69323b600688546cb4fe61a
Marc <valente.marc@gmail.com>
2014-05-10 10:53:58 -0700

    fist commit for tesitng pull request

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PullTest::Application.config.secret_key_base = '6dcdaebdd01e73430e4a4d9242c87749147bbe52a6f85bae48c46c3b76d8980df51e69f1e2f7847dfd8c0d9b87fcc110ac1af43c882b28481a43ba368446ef77'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PullTest::Application.config.session_store :cookie_store, key: '_pull_test_session'
